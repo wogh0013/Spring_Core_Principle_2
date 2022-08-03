71223335f05f17908a9d57ddc1bb6246e0e1d315
username <wogh0013@naver.com>
2022-08-03 23:19:35 +0900

    commit

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,39 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름없이 타입만으로 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanByNameX(){
+        //파라미터2가 실행 시 -> 파라미터1 예외 발생
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class));
+    }
+}
---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,54 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer(){
+        AppConfig appConfig = new AppConfig();
+
+        //1. 조회 : 호출할 때마다 객체 생성
+        MemberService memberService1 = appConfig.memberService();
+        //2. 조회 : 호출할 때마다 객체 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조값이 다른 것 확인
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest(){
+        //1. 조회 : 호출할 때마다 객체 생성
+        SingletonService instance1 = SingletonService.getInstance();
+        //2. 조회 : 호출할 때마다 객체 생성
+        SingletonService instance2 = SingletonService.getInstance();
+
+        //참조값이 같은 것 확인
+        assertThat(instance1).isSameAs(instance2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회 호출할 때마다 같은 객체 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        //2. 조회 : 호출할 때마다 같은 객체 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //3. 참조값이 같은 것을 확인
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+}
